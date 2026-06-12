4801287ba4cad26cb64730b5c9e6d21fafc1894c
ColFrozenLiquid <colfrozenliquid@gmail.com>
2026-06-12 20:31:36 +0200

    add ring buffer

---
+++ ringbuffer.c
@@ -0,0 +1,55 @@
+#include "ringbuffer.h"
+#include <bits/types/struct_iovec.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void rb_init(RingBuffer* rb) {
+    rb->head = 0;
+    rb->tail = 0;
+    rb->count = 0;
+}
+
+void rb_write(RingBuffer* rb, const uint8_t* data, size_t len) {
+    if (rb_free_space(rb) < len) {
+        fprintf(stderr, "Ring buffer is full. This should never happen. Increase size of ring buffer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        rb->data[rb->head] = data[i];
+        rb->head = (rb->head + 1) % RING_BUFFER_SIZE;
+    }
+    rb->count += len;
+}
+
+// 1. Read the first 4 bytes to get the prefixed message length
+// 2. Check if the message can be read in one contigous chunk
+// 3. Copy message to destination buffer
+// 4. Decrement count and move tail position forward
+void rb_consume(RingBuffer* rb, uint8_t* dst) {
+    uint32_t data_len = ((uint32_t)rb->data[rb->tail] << 24)
+        |   ((uint32_t)rb->data[(rb->tail + 1) % RING_BUFFER_SIZE] << 16)
+        |   ((uint32_t)rb->data[(rb->tail + 2) % RING_BUFFER_SIZE] << 8)
+        |   ((uint32_t)rb->data[(rb->tail + 3) % RING_BUFFER_SIZE]);
+
+    size_t start = (rb->tail + 4) % RING_BUFFER_SIZE;
+
+    size_t first_chunk = RING_BUFFER_SIZE - start;
+    if (first_chunk > data_len) {
+        first_chunk = data_len;
+    }
+
+    memcpy(dst, rb->data + start, first_chunk);
+    if (data_len > first_chunk) {
+        memcpy(dst + first_chunk, rb->data, data_len - first_chunk);
+    }
+
+    rb->tail = (rb->tail + 4 + data_len) % RING_BUFFER_SIZE;
+    rb->count -= (4 + data_len);
+}
+
+size_t rb_free_space(RingBuffer* rb) {
+    return RING_BUFFER_SIZE - rb->count;
+}
---
+++ ringbuffer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <bits/types/struct_iovec.h>
+#include <stdint.h>
+
+#define RING_BUFFER_SIZE 8192   // 8KB
+
+typedef struct {
+    uint8_t data[RING_BUFFER_SIZE];
+    size_t head;
+    size_t tail;
+    size_t count;
+} RingBuffer;
+
+void rb_init(RingBuffer* rb);
+
+void rb_write(RingBuffer* rb, const uint8_t* data, size_t len);
+
+size_t rb_free_space(RingBuffer* rb);
+
+void rb_consume(RingBuffer* rb, uint8_t* dst);
